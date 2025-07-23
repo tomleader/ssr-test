36ba88c6bc902f7195cb7e3fdd5610ebb6e735f2
linktang <tomleader4444@gmail.com>
2025-07-23 16:47:00 +0800

    Update hello.js

--- node-functions/basic/hello.js
+++ node-functions/basic/hello.js
@@ -1,3 +1,13 @@
+async function sleep(time) {
+  time = time || 5000;
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, time)
+  })
+}
+
 export async function onRequest(context) {
+  await sleep();
   return new Response("Hello, world!")
 } 
